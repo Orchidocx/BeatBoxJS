cb985e4b6c8b6f6c404464aabfd683aea25e70f2
Jacky Li <ilikemacaronii@gmail.com>
2020-10-07 01:57:12 -0700

    finished exercise

--- utils.js
+++ utils.js
@@ -18,20 +18,40 @@ class Beat {
  * Button class that keeps track of the button color based on a press
  */
 class Button {
-  constructor(color, keyCode) {}
+  constructor(color, keyCode) {
+    this.color = color;
+    this.keyCode = keyCode;
+    this.element = document.getElementById(keyCode);
+    this.setButtonColorInHTML();
+    this.setTransitionEndListener();
+  }
 
   /**
    * Set the button color based on color specified
    */
-  setButtonColorInHTML = () => {};
+  setButtonColorInHTML = () => {
+    this.element.style.borderColor = this.color;
+  };
+
+  setTransitionEndListener = () => {
+    this.element.addEventListener("transitionend", () => {
+      this.deselect();
+    });
+  };
 
   /**
    * Select function to set the background color and boxShadow
    */
-  select = () => {};
+  select = () => {
+    this.element.style.backgroundColor = this.color;
+    this.element.style.boxShadow = `0 0 17px 0 ${this.color}`;
+  };
 
   /**
    * Deselect function to reset background color and boxShadow
    */
-  deselect = () => {};
+  deselect = () => {
+    this.element.style.backgroundColor = "transparent";
+    this.element.style.boxShadow = "none";
+  };
 }
